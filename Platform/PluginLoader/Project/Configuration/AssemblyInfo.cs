42672d1f9c1fccd8db6e8a088c19c7aa336175f7
AutoBuild <wayne@tickzoom.com>
2011-07-31 11:14:21 -0400

    Build version 1.2.36.36354.

--- Platform/PluginLoader/Project/Configuration/AssemblyInfo.cs
+++ Platform/PluginLoader/Project/Configuration/AssemblyInfo.cs
@@ -19,4 +19,4 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.2.35.36074")]
+[assembly: AssemblyVersion("1.2.36.36354")]
--- Platform/TickZoomGui2/Properties/AssemblyInfo.cs
+++ Platform/TickZoomGui2/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.35.36074")]
+[assembly: AssemblyVersion("1.2.36.36354")]
 //[assembly: AssemblyFileVersion("2.0.0.0")]
