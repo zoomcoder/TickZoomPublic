51beff37597fd6e623b6d57c60594237e39d006e
AutoBuild <wayne@tickzoom.com>
2011-08-16 09:31:23 -0400

    Build version 1.2.38.38648.

--- Platform/PluginLoader/Test/AssemblyInfo.cs
+++ Platform/PluginLoader/Test/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.CompilerServices;
 // You can specify all values by your own or you can build default build and revision
 // numbers with the '*' character (the default):
 
-[assembly: AssemblyVersion("1.2.38.38641")]
+[assembly: AssemblyVersion("1.2.38.38648")]
 
 // The following attributes specify the key for the sign of your assembly. See the
 // .NET Framework documentation for more information about signing.
--- Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
+++ Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.38.38641")]
+[assembly: AssemblyVersion("1.2.38.38648")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
