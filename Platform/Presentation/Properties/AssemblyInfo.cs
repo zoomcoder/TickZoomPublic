bf5224da9858e69ee7a5fe1f1b00a3a56cb654d7
AutoBuild <wayne@tickzoom.com>
2011-10-12 10:17:45 -0400

    Build version 1.2.46.46861.

--- Platform/Presentation/Properties/AssemblyInfo.cs
+++ Platform/Presentation/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.44.44858")]
+[assembly: AssemblyVersion("1.2.46.46861")]
--- Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
+++ Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.44.44858")]
+[assembly: AssemblyVersion("1.2.46.46861")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
