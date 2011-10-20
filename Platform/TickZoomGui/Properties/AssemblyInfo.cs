ff42cf142569cb3e505f2e433d5c1fce2a404eae
AutoBuild <wayne@tickzoom.com>
2011-10-20 14:59:56 -0400

    Build version 1.2.47.48042.

--- Platform/TickZoomGui/Properties/AssemblyInfo.cs
+++ Platform/TickZoomGui/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using TickZoom.Api;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.47.47882")]
+[assembly: AssemblyVersion("1.2.47.48042")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
