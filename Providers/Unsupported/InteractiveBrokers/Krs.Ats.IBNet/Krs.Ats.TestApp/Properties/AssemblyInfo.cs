18babd44c83f1a443e7601005aed434c29049d76
AutoBuild <wayne@tickzoom.com>
2011-11-11 18:09:31 -0500

    Build version 1.2.50.51227.

--- Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
+++ Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.50.51043")]
+[assembly: AssemblyVersion("1.2.50.51227")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
