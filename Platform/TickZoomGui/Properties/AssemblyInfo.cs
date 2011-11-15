3a15dd0d834a38af315eaf5ca3964541dfa4d091
unknown <wayne@.(none)>
2011-11-15 10:03:11 -0500

    Build version 1.2.51.51756.

--- Platform/TickZoomGui/Properties/AssemblyInfo.cs
+++ Platform/TickZoomGui/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using TickZoom.Api;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.50.51227")]
+[assembly: AssemblyVersion("1.2.51.51756")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
