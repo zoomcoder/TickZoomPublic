14aaafc1e698b17703d5bd92a7d36613e6a6b5c1
Wayne Walter <waynewalter@tickzoom.com>
2011-12-11 21:15:32 -0500

    Handle ThreadStateException when capturing stack traces of all threads.

--- Providers/Common/ProviderUtil/FIX/FIXSimulatorSupport.cs
+++ Providers/Common/ProviderUtil/FIX/FIXSimulatorSupport.cs
@@ -262,7 +262,7 @@ namespace TickZoom.FIX
                     throw new ApplicationException("HeartBeat response was never received.");
                 }
                 isHeartbeatPending = TimeStamp.UtcNow;
-                isHeartbeatPending.AddSeconds(5);
+                isHeartbeatPending.AddSeconds(15);
                 OnHeartbeat();
             }
             currentTime.AddSeconds(1);
