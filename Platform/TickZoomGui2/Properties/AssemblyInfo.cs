2c5f4a149cd4543ae914b7a8c83881c555cff498
unknown <wayne@.(none)>
2011-12-21 13:56:38 -0500

    Build version 1.2.56.56963.

--- Platform/TickZoomGui2/Properties/AssemblyInfo.cs
+++ Platform/TickZoomGui2/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.56.56959")]
+[assembly: AssemblyVersion("1.2.56.56963")]
 //[assembly: AssemblyFileVersion("2.0.0.0")]
