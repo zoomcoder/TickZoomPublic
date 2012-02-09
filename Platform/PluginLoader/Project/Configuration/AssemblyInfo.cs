d77219aeb9f2d7ed6c478391072fa8cb6ebecfa6
unknown <wayne@.(none)>
2012-02-09 00:28:44 -0500

    Build version 1.2.63.64083.

--- Platform/PluginLoader/Project/Configuration/AssemblyInfo.cs
+++ Platform/PluginLoader/Project/Configuration/AssemblyInfo.cs
@@ -19,4 +19,4 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.2.62.63020")]
+[assembly: AssemblyVersion("1.2.63.64083")]
--- Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
+++ Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.62.63020")]
+[assembly: AssemblyVersion("1.2.63.64083")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
