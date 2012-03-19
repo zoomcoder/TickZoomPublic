f63d9d73f03b2f05db727e8978355239c7377bc8
unknown <wayne@.(none)>
2012-03-19 10:12:16 -0400

    Build version 1.3.0.925.

--- Platform/PluginLoader/Project/Configuration/AssemblyInfo.cs
+++ Platform/PluginLoader/Project/Configuration/AssemblyInfo.cs
@@ -19,4 +19,4 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.3.0.921")]
+[assembly: AssemblyVersion("1.3.0.925")]
