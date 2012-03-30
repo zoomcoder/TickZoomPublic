c56817b141841669feaa9dd7f85ebd1252f8fdce
unknown <wayne@.(none)>
2012-03-30 16:18:55 -0400

    Build version 1.3.2.2546.

--- Platform/TickZoomGui/Properties/AssemblyInfo.cs
+++ Platform/TickZoomGui/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using TickZoom.Api;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.2.2251")]
+[assembly: AssemblyVersion("1.3.2.2546")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
+++ Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.2.2251")]
+[assembly: AssemblyVersion("1.3.2.2546")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
